d240217f6042496d3b8416c66b31d00cc162bece
SHOZO KUZE <shozo@kuze.jp>
2018-02-04 14:40:26 +0900

    2018/2/4 ほのお、ガンバレ！！

---
+++ animation_data/fire_ganbare.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   fire_ganbare.h
+ * Author: MATHRAX
+ *
+ * Created on February 4, 2018, 2:40 PM
+ */
+
+#ifndef FIRE_GANBARE_H
+#define	FIRE_GANBARE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* FIRE_GANBARE_H */
+
